7ad163498967d91e4c1b62744b850683414e0b44
Madhushan11 <139599634+Madhushan11@users.noreply.github.com>
2024-06-28 00:50:56 +0530

    Create pipeline.groovy

---
+++ pipeline.groovy
@@ -0,0 +1,41 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('SCM checkout') {
+            steps {
+                retry(3) {
+                    checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/Madhushan11/Backend.git']])
+                }
+            }
+        }
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    sh 'docker build -t Shakila/frontend-app-image .'
+                }
+            }
+        }
+        stage('Run Docker Container') {
+            steps {
+                script {
+                    // Stop any running containers with the same name
+                    
+                    // Run the new container
+                    sh 'docker run -d -p 3001:3000 Shakila/frontend-app-image'
+                }
+            }
+        }
+        stage('Show Running Containers') {
+            steps {
+                sh 'docker ps'
+            }
+        }
+    }
+
+    post {
+        always {
+            cleanWs()
+        }
+    }
+}
